8c36eb0d060f636ac011c292cdb4ae96bb610a3d
ymerenich1998 <ymerenich1998@gmail.com>
2025-07-13 11:07:27 +0300

    Add life display and update win/lose conditions; adjust ammo regeneration logic

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ fire_sound.set_volume(0.1)
 font.init()
 stat_font = font.SysFont("Arial", 24)
 result_font = font.SysFont("Arial", 36)
+life_font = font.SysFont("Arial", 72)
 
 win_text = result_font.render("You win!", True, (0, 255, 0))
 lose_text = result_font.render("You lose!", True, (255, 0, 0))
@@ -111,13 +112,10 @@ while run:
   if not finished:
     win.blit(bg, (0, 0))
 
-    # win.blit(win_text, (win_width // 2 - win_text.get_width() // 2, win_height // 2 - win_text.get_height() // 2))
-    # win.blit(lose_text, (win_width // 2 - lose_text.get_width() // 2, win_height // 2 - lose_text.get_height() // 2))
-
     # Display score and lost count
     text_score = stat_font.render(f"Score: {score}", True, (255, 255, 255))
     text_lost = stat_font.render(f"Lost: {lost}", True, (255, 255, 255))
-    text_life = stat_font.render(f"Life: {life}", True, (255, 255, 255))
+    text_life = life_font.render(f"{life}", True, (123, 225, 100))
     win.blit(text_score, (10, 10))
     win.blit(text_lost, (10, 50))
     win.blit(text_life, (650, 10))
@@ -126,6 +124,14 @@ while run:
       finished = True
       win.blit(lose_text, (win_width // 2 - lose_text.get_width() // 2, win_height // 2 - lose_text.get_height() // 2))
     
+    if lost >= 10:
+      finished = True
+      win.blit(lose_text, (win_width // 2 - lose_text.get_width() // 2, win_height // 2 - lose_text.get_height() // 2))
+
+    if score >= 30:
+      finished = True
+      win.blit(win_text, (win_width // 2 - win_text.get_width() // 2, win_height // 2 - win_text.get_height() // 2))
+   
     # Update and draw player and enemies
     player.update()
     enemies.update()
@@ -139,7 +145,9 @@ while run:
     for _ in collides:
       score += 1
       if ammo < max_ammo:
-        ammo += 1 
+        chance = randint(0, 1)
+        if chance == 1:
+          ammo += 1
       enemy = Enemy("ufo.png", randint(80, win_width - 80), randint(-100, -40), randint(1, 3), 80, 40)
       enemies.add(enemy)
     
@@ -159,7 +167,7 @@ while run:
       ammo_text = stat_font.render("Out of Ammo!", True, (255, 0, 0))
       win.blit(ammo_text, (10, 90))
 
-    if time.get_ticks() % 120 == 0 and ammo < max_ammo:
+    if time.get_ticks() % 150 == 0 and ammo < max_ammo:
       ammo += 1
 
   display.update()
